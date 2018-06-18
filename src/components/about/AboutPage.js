d117d4aa037962ae363e013c051645012503eb7b
aaaaeeee <36264276+aaaaeeee@users.noreply.github.com>
2018-06-18 13:36:52 +0300

    Initial commit

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class AboutPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This application uses React, Redux, React Router!</p>
+
+      </div>
+    );
+  }
+}
+
+
+export default AboutPage;
---
+++ src/components/book/BooksPage.js
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+
+
+class BooksPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Books</h1>
+      </div>
+    );
+  }
+}
+
+export default BooksPage;
